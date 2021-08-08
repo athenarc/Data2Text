81c92168ccbac3287b0a20fb3e4774a5db83ca61
MikeXydas <mikexydas@gmail.com>
2021-08-08 16:42:18 +0300

    Representing wandb table row via a dataclass. Changed bleu calculation from nlp to nltk (slower but no initialization is needed).

--- visualizing/wandb_table.py
+++ visualizing/wandb_table.py
@@ -1,23 +1,36 @@
+import warnings
+from dataclasses import dataclass, field
 from typing import List, Tuple  # Typing
 
 import wandb
-from nlp import Metric, load_metric
+from nltk.translate.bleu_score import sentence_bleu
 from wandb.wandb_run import Run  # Typing
 
 
-def calculate_inf_row(inference: str, target: str, source: str, metric: Metric) -> Tuple[str, str, str, float]:
-    score = metric.compute([inference], [[target]])['bleu']
+@dataclass
+class TableRow:
+    predicted: str
+    target: str
+    source: str
+    bleu: float = field(init=False)
 
-    return inference, target, source, score
+    def to_tuple(self):
+        return self.predicted, self.target, self.source, self.bleu
+
+    def __post_init__(self):
+        with warnings.catch_warnings():
+            # Ignoring specific warning of calculating bleu on small sentences
+            warnings.simplefilter("ignore")
+            self.bleu = sentence_bleu([self.target], self.predicted)
 
 
 def create_inference_examples_table(run: Run, inferences_targets: List[List[str]], sources: List[str]) -> None:
     inf_table = wandb.Table(columns=["Predicted", "Target", "Source", "BLEU"])
-    bleu_metric = load_metric('bleu', experiment_id="evaluate")
 
     zipped_inf_target_source = [[*inf_target, source] for inf_target, source in zip(inferences_targets, sources)]
 
     for inference, target, source in zipped_inf_target_source:
-        inf_table.add_data(*calculate_inf_row(inference, target, source, bleu_metric))
+        table_row = TableRow(inference, target, source)
+        inf_table.add_data(*table_row.to_tuple())
 
     run.log({"inference_samples": inf_table})
