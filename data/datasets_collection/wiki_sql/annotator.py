93ccd974080e20876e072b5d19f58690491719bd
MikeXydas <mikexydas@gmail.com>
2021-09-01 16:43:43 +0300

    Seperated Annotator logic and Annotator view

--- data/datasets_collection/wiki_sql/annotator.py
+++ data/datasets_collection/wiki_sql/annotator.py
@@ -2,21 +2,7 @@ import json
 import random
 from collections import defaultdict
 
-import pandas as pd
 from db_interface import WikiSqlController
-
-
-def user_confirm_output(output):
-    print()
-    with pd.option_context('display.max_rows', None,
-                           'display.max_columns', None,
-                           'display.width', 1_000):
-        print(output)
-    answer = ""
-    while answer != "yes" and answer != "no":
-        answer = input("> Is the above output expected (yes/no): ")
-
-    return answer == "yes"
 
 
 class Annotator:
@@ -59,58 +45,20 @@ class Annotator:
     def propose_table(self):
         table_id = random.choice(self.table_pool)
         full_table = self.wiki_interface.select_all(table_id)
-
+        full_table.columns = [f"{ind}.{col}" for (ind, col) in enumerate(full_table.columns)]
         return table_id, full_table
 
-    def save_annotations(self, table_id, query, desc, result_df):
+    def save_annotations(self, table_id, query, desc_query, desc_results, result_df):
         annotation = {
             'table_id': table_id,
             'query': query,
-            'description': desc,
+            'query_description': desc_query,
+            'results_description': desc_results,
             'result': result_df.to_csv()
         }
         self.annotations.append(annotation)
         with open(self.json_save_path, 'w') as fp:
             json.dump(self.annotations, fp)
 
-    def annotation_loop(self):
-        exit_annotation = False
-        while not exit_annotation:
-            # Step 1: Propose and accept a table
-            while True:
-                table_id, full_table = self.propose_table()
-                sql_table_id = f"table_{table_id.replace('-', '_')}"
-                if user_confirm_output(full_table):
-                    break
-
-            # Step 2: Ask the user to write a query
-            while True:
-                query = input("> Query (use col indexes and table_id): ")
-                query = query.replace("table_id", sql_table_id)
-
-                print(query)
-                res = self.execute_input_query(query)
-                if user_confirm_output(res):
-                    break
-
-            # Step 3: Ask the user to describe the query
-            while True:
-                desc = input("> Query description: ")
-                if user_confirm_output(desc):
-                    break
-
-            # Step 4: Save the annotation
-            self.save_annotations(table_id, query, desc, res)
-
-            print("> Annotation saved.")
-            print()
-
-
-if __name__ == '__main__':
-    # Paths will work if working directory is the directory of this file
-    WIKISQL_DB_PATH = "../../../storage/datasets/wiki_sql/raw/train.db"
-    WIKISQL_JSON_PATH = "../../../storage/datasets/wiki_sql/raw/train.tables.jsonl"
-    QUERIES_PATH = "../../../storage/datasets/wiki_sql/raw/train.jsonl"
-    ANNOTATION_STORAGE_PATH = "../../../storage/datasets/wiki_sql/annotations/train.json"
-    annotator = Annotator(WIKISQL_DB_PATH, WIKISQL_JSON_PATH, QUERIES_PATH, ANNOTATION_STORAGE_PATH)
-    annotator.annotation_loop()
+    def get_annotations_numb(self):
+        return len(self.annotations)
---
+++ data/datasets_collection/wiki_sql/annotator_view.py
@@ -0,0 +1,115 @@
+import sqlite3
+
+import pandas as pd
+from annotator import Annotator
+from termcolor import colored
+
+
+def user_confirm_output(output):
+    print()
+    with pd.option_context('display.max_rows', None,
+                           'display.max_columns', None,
+                           'display.width', 1_000):
+        print(output)
+    answer = ""
+    while answer != "y" and answer != "n":
+        answer = input(colored("> Is the above output expected (y/n): ", color="green"))
+    print()
+    return answer == "y"
+
+
+class AnnotatorView:
+    def __init__(self, db_path, table_info_path, queries_path, json_save_path):
+        self.annotator = Annotator(db_path, table_info_path, queries_path, json_save_path)
+
+        # Initial logging to make sure that the configuration is correct
+        self.show_welcome_message()
+        self.show_settings(db_path, table_info_path, queries_path, json_save_path)
+
+    def annotation_loop(self):
+        exit_annotation = False
+
+        while not exit_annotation:
+            # Step 1: Propose and accept a table
+            print(colored("####### Step 1: Table Proposal #######", color='cyan'))
+            while True:
+                table_id, full_table = self.annotator.propose_table()
+                sql_table_id = f"table_{table_id.replace('-', '_')}"
+                if user_confirm_output(full_table):
+                    break
+
+            # Step 2: Ask the user to write a query
+            print(colored("####### Step 2: Query Proposal #######", color='cyan'))
+            while True:
+                query = input("> Query (use col indexes and table_id): ")
+                query = query.replace("table_id", sql_table_id)
+
+                print(f"> Running query: {query}")
+                try:
+                    res = self.annotator.execute_input_query(query)
+                except (AttributeError, SyntaxError, sqlite3.OperationalError):
+                    print(colored("> Input query could not be parsed. Check the syntax and retry.\n", color='red'))
+                    continue
+                if user_confirm_output(res):
+                    break
+
+            # Step 3: Ask the user to describe the query
+            print(colored("####### Step 3: Query Description #######", color='cyan'))
+            while True:
+                desc_query = input("> Query description: ")
+                if user_confirm_output(desc_query):
+                    break
+
+            # Step 4: Ask the user to describe the results of the query
+            print(colored("####### Step 4: Results Description #######", color='cyan'))
+            while True:
+                desc_results = input("> Results description: ")
+                if user_confirm_output(desc_results):
+                    break
+
+            # Step 5: Save the annotation
+            print(colored("####### Step 5: Save Annotation #######", color='cyan'))
+            self.show_annotation(table_id, query, desc_query, desc_results, res)
+            self.annotator.save_annotations(table_id, query, desc_query, desc_results, res)
+
+            # Step 6: Check if finished
+            exit_msg = input(colored(f"Total annotations: {self.annotator.get_annotations_numb()}. "
+                                     f"Would you like to exit? (y/n)", color='green'))
+            exit_annotation = exit_msg == "y"
+
+    @staticmethod
+    def show_welcome_message():
+        print(colored("\n"
+                      "############################################\n"
+                      "              WikiSQL Annotator             \n"
+                      "############################################\n"
+                      , color="green"))
+
+    def show_settings(self, db_path, table_info_path, queries_path, json_save_path):
+        print(colored("> Annotator configuration:", color='yellow'))
+        print(colored(f"\t* Database: {db_path}", color='yellow'))
+        print(colored(f"\t* Table info: {table_info_path}", color='yellow'))
+        print(colored(f"\t* Queries: {queries_path}", color='yellow'))
+        print(colored(f"\t* Annotation storage: {json_save_path}", color='yellow'))
+        print(colored(f"\t* Annotations so far: {self.annotator.get_annotations_numb()}", color="cyan"))
+        print()
+
+    @staticmethod
+    def show_annotation(table_id, query, desc_query, desc_results, res):
+        print("> The following annotation will be stored:")
+        print(f"\t* Table Id: {table_id}")
+        print(f"\t* Query: {query}")
+        print(f"\t* Query Description: {desc_query}")
+        print(f"\t* Results Description: {desc_results}")
+        print(f"\t* Result rows: {len(res)}")
+        input("> Press enter to save...")
+
+
+if __name__ == '__main__':
+    # Paths will work if working directory is the directory of this file
+    WIKISQL_DB_PATH = "../../../storage/datasets/wiki_sql/raw/train.db"
+    WIKISQL_JSON_PATH = "../../../storage/datasets/wiki_sql/raw/train.tables.jsonl"
+    QUERIES_PATH = "../../../storage/datasets/wiki_sql/raw/train.jsonl"
+    ANNOTATION_STORAGE_PATH = "../../../storage/datasets/wiki_sql/annotations/train.json"
+    annotator = AnnotatorView(WIKISQL_DB_PATH, WIKISQL_JSON_PATH, QUERIES_PATH, ANNOTATION_STORAGE_PATH)
+    annotator.annotation_loop()
