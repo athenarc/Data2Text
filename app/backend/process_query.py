f540224e6c7f3e3a672d1270b96565960874251f
MikeXydas <mikexydas@gmail.com>
2021-09-08 10:05:07 +0300

    Replaced str.find() with str.index().

--- app/backend/process_query.py
+++ app/backend/process_query.py
@@ -21,17 +21,17 @@ def cols_added_to_sel(sel_cols: List[str], where_cols: List[str]) -> List[str]:
 
 def find_query_clauses(query: str) -> Tuple[str, str, str]:
     lowered_query = query.lower()
-    sel_clause_start = lowered_query.find("select") + len("select") + 1
-    sel_clause_end = lowered_query.find("from") - 1
+    sel_clause_start = lowered_query.index("select") + len("select") + 1
+    sel_clause_end = lowered_query.index("from") - 1
 
-    from_clause_start = lowered_query.find("from") + len("from") + 1
-    from_clause_end = lowered_query.find("where") - 1
-
-    if from_clause_end == -2:
+    from_clause_start = lowered_query.index("from") + len("from") + 1
+    try:
+        # The query might not have a where clause
+        from_clause_end = lowered_query.index("where") - 1
+    except ValueError:
         return query[sel_clause_start:sel_clause_end], query[from_clause_start:], ""
 
-    where_clause_start = lowered_query.find("where") + len("where") + 1
-    # where_clause_end = -1
+    where_clause_start = lowered_query.index("where") + len("where") + 1
 
     return query[sel_clause_start:sel_clause_end], query[from_clause_start:from_clause_end], \
            query[where_clause_start:]
