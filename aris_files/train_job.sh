114f11c55a135c3ccc33e47305f97301d4b80e71
MikeXydas <mikexydas@gmail.com>
2021-10-04 13:24:39 +0300

    Added a human evaluation pipeline.

---
+++ aris_files/train_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=debug_train # Job name
+#SBATCH --output=gpujob.%j.out # Stdout (%j expands to jobId)
+#SBATCH --error=gpujob.%j.err # Stderr (%j expands to jobId)
+#SBATCH --ntasks=1 # Total number of tasks
+#SBATCH --gres=gpu:1 # GPUs per node
+#SBATCH --nodes=1 # Total number of nodes requested
+#SBATCH --ntasks-per-node=1 # Tasks per node
+#SBATCH --cpus-per-task=10 # Threads per task
+#SBATCH --mem=12000 # Memory per job in MB
+#SBATCH -t 00:30:00 # Run time (hh:mm:ss) - (max 48h)
+#SBATCH --partition=gpu # Run on the GPU nodes queue
+#SBATCH -A pa210903 # Accounting project
+
+# Load any necessary modules
+module purge
+module load gnu/8
+module load cuda/10.1.168
+module load pytorch/1.8.0
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Launch the executable
+#pip install -r aris_requirements.txt
+cd $HOME/data2text
+source venv/bin/activate
+
+srun python3.8 entry_point.py --config_file configs/full_totto.yaml --job_type train
+#srun python3 test.py
---
+++ tools/inference_human_evaluation.py
@@ -0,0 +1,114 @@
+"""
+This cmd tool inputs an inference table downloaded from wandb (.json). It then
+iteratively requests human evaluation for each one of the inferences.
+
+Currently, the evaluator can make one of the following choices to evaluate the inference:
+    1. Correct
+    2. Confusion (confused on what some column name/value means)
+    3. Omission (a column was not outputted in the inference
+    4. Erroneous (a non-confusion mistake was made)
+    5. Hallucination (the model add its own wrong info)
+    6. Other, (specify)
+"""
+import argparse
+import json
+import os
+
+from termcolor import colored
+
+AVAILABLE_CHOICES = {
+    1: "Correct",
+    2: "Confusion",
+    3: "Omission",
+    4: "Erroneous",
+    5: "Hallucination",
+    6: "Other"
+}
+
+
+def parse_inference(inference_table):
+    return {
+        "target": inference_table[1],
+        "predicted": inference_table[0],
+        "source": inference_table[5]
+    }
+
+
+def show_inference(inference):
+    print(colored("> Available choices:", color="cyan"))
+    for ind, choice in AVAILABLE_CHOICES.items():
+        print(f"{ind}. {choice}")
+    print()
+    print(f"{colored('Target', color='cyan')}: {inference['target']}")
+    print(f"{colored('Predicted', color='cyan')}: {inference['predicted']}")
+    print(f"{colored('Source', color='cyan')}: {inference['source']}")
+    print()
+
+
+def confirm_choice(evaluation):
+    while True:
+        print(f"> {colored('Chosen evaluation:', color='cyan')} {evaluation}")
+        confirmed_str = input(colored("Confirm (y/n): ", color="green"))
+
+        if confirmed_str.lower() == "y":
+            return True
+        elif confirmed_str.lower() == "n":
+            return False
+        else:
+            continue
+
+
+def choice_loop():
+    while True:
+        try:
+            choice = int(input(colored("> Choice: ", color="cyan")))
+            if choice < 1 or choice > len(AVAILABLE_CHOICES.items()):
+                raise ValueError
+        except ValueError:
+            print(f"Unexpected input, choose an integer from 1-{len(AVAILABLE_CHOICES.items())}")
+            continue
+
+        evaluation = AVAILABLE_CHOICES[choice]
+        if AVAILABLE_CHOICES[choice] == "Other":
+            evaluation += f", {input(colored('> Explanation: ', color='cyan'))}"
+
+        if not confirm_choice(evaluation):
+            continue
+
+        return evaluation
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Inference human evaluator.")
+    parser.add_argument(
+        "-i", "--inferences", required=True, help="path to inferences file", type=str,
+    )
+    parser.add_argument(
+        "-o", "--output", required=True, help="path to output human evaluation file (should not exist).", type=str,
+    )
+    args = parser.parse_args()
+
+    with open(args.inferences) as f:
+        inferences = json.load(f)["data"]
+
+    if os.path.exists(args.output):
+        raise FileExistsError("Output file should not exist.")
+
+    evaluated_inferences = []
+    for raw_inference in inferences:
+        print("\n---------------------------------------------------------------------")
+        inference = parse_inference(raw_inference)
+        show_inference(inference)
+
+        evaluation = choice_loop()
+        inference['evaluation'] = evaluation
+        evaluated_inferences.append(inference)
+
+        with open(args.output, 'w') as fp:
+            json.dump(evaluated_inferences, fp)
+
+        print(f"Evaluations done: {len(evaluated_inferences)} / {len(inferences)}")
+
+
+if __name__ == '__main__':
+    main()
