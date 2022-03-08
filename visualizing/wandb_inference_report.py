520a0883eb6b898c75b733f6a7d576bb9daa11bb
MikeXydas <mikexydas@gmail.com>
2022-03-08 19:58:06 +0200

    Calaculating GRUEN per sentence

--- visualizing/wandb_inference_report.py
+++ visualizing/wandb_inference_report.py
@@ -6,9 +6,10 @@ from typing import Dict, List, Tuple  # Typing
 import datasets
 import transformers
 import wandb
+from tqdm import tqdm
 from wandb.wandb_run import Run  # Typing
 
-from visualizing.gruen.gruen_original import get_gruen
+from visualizing.gruen.gruen_calculation import Gruen
 from visualizing.parent.parent_calc import parent_calc
 from visualizing.parent.table_process import tables_to_parent_format
 from visualizing.totto_table_parse import to_valid_html
@@ -22,14 +23,16 @@ class InferenceEvaluation:
     bleu: float = field(init=False)
     bertscore: float = field(init=False)
     parent: float = field(init=False)
+    gruen: float = field(init=False)
     source: str
 
     # Attributes below are helpers and not logged in the table
     tokenizer: InitVar[transformers.PreTrainedTokenizer] = None
     bleu_calculator: InitVar[datasets.metric.Metric] = None
     bertscore_calculator: InitVar[datasets.metric.Metric] = None
+    gruen_calculator: InitVar[Gruen] = None
 
-    def __post_init__(self, tokenizer, bleu_calculator, bertscore_calculator):
+    def __post_init__(self, tokenizer, bleu_calculator, bertscore_calculator, gruen_calculator):
         self.bleu = bleu_calculator.compute(predictions=[tokenizer.tokenize(self.predicted)],
                                             references=[[tokenizer.tokenize(targets) for targets in self.targets]])['bleu']
 
@@ -43,19 +46,21 @@ class InferenceEvaluation:
         except ValueError:
             self.parent = -1
 
+        self.gruen = gruen_calculator.compute(predictions=[self.predicted])[0]
+
     def to_tuple(self):
         return self.predicted, " | ".join(self.targets), \
                self.html_source, self.bleu, self.bertscore, \
-               self.parent, self.source
+               self.parent, self.gruen, self.source
 
     @staticmethod
     def get_field_names_in_order() -> List[str]:
         """ There is hard coupling between this function and to_tuple above"""
-        return ["Predicted", "Target", "HTML Source", "BLEU", "BertScore", "PARENT", "Source"]
+        return ["Predicted", "Target", "HTML Source", "BLEU", "BertScore", "PARENT", "GRUEN", "Source"]
 
     def get_float_metrics(self) -> Dict[str, float]:
         """ Returns the metrics that can then be aggregated """
-        return {"bleu": self.bleu, "bertscore": self.bertscore, "PARENT": self.parent}
+        return {"bleu": self.bleu, "bertscore": self.bertscore, "PARENT": self.parent, "GRUEN": self.gruen}
 
 
 def create_inference_examples_table(inference_evaluations: List[InferenceEvaluation]):
@@ -83,15 +88,17 @@ def create_inferences_evaluations(zipped_inf_targets_source: List[Tuple[str, Lis
     # Initialize 🤗 datasets metrics
     bleu_calculator = datasets.load_metric('bleu', experiment_id="debug")
     bertscore_calculator = datasets.load_metric('bertscore', experiment_id="debug")
+    gruen_calculator = Gruen('storage/checkpoints/metrics/cola/')
 
     inference_evaluations = []
-    for inference, targets, source in zipped_inf_targets_source:
+    for inference, targets, source in tqdm(zipped_inf_targets_source, desc="Metric calculation:"):
         inference_eval = InferenceEvaluation(inference, targets,
                                              wandb.Html(to_valid_html(source)),
                                              source,
                                              tokenizer=tokenizer,
                                              bleu_calculator=bleu_calculator,
-                                             bertscore_calculator=bertscore_calculator)
+                                             bertscore_calculator=bertscore_calculator,
+                                             gruen_calculator=gruen_calculator)
         inference_evaluations.append(inference_eval)
 
     return inference_evaluations
@@ -110,9 +117,6 @@ def create_inference_report_on_wandb(run: Run, inferences: List[str], targets: L
 
     # Aggregated metrics on the whole evaluation set
     aggregated_metrics = calculate_aggregated_metrics(inference_evaluations)
-
-    # Inject the GRUEN metric (so far it is not calculated
-    aggregated_metrics['GRUEN'] = float(sum(get_gruen(inferences)) / len(inferences))
 
     # Log the results on wandb
     run.log({"inference_samples": inference_examples_on_table})
