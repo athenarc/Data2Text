378747072ffcd729751a481a45aa902a12bf568f
MikeXydas <mikexydas@gmail.com>
2022-04-16 11:50:12 +0300

    Added t5_base pretraining config

--- aris_files/pretrain_job.sh
+++ aris_files/pretrain_job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=debug_pretrain # Job name
+#SBATCH --job-name=pretrain # Job name
 #SBATCH --output=gpujob.%j.out # Stdout (%j expands to jobId)
 #SBATCH --error=gpujob.%j.err # Stderr (%j expands to jobId)
 #SBATCH --ntasks=1 # Total number of tasks
-#SBATCH --gres=gpu:2 # GPUs per node
+#SBATCH --gres=gpu:1 # GPUs per node
 #SBATCH --nodes=1 # Total number of nodes requested
 #SBATCH --ntasks-per-node=1 # Tasks per node
 #SBATCH --cpus-per-task=10 # Threads per task
-#SBATCH --mem=12000 # Memory per job in MB
-#SBATCH -t 01:00:00 # Run time (hh:mm:ss) - (max 48h)
+#SBATCH --mem=47000 # Memory per job in MB
+#SBATCH -t 47:00:00 # Run time (hh:mm:ss) - (max 48h)
 #SBATCH --partition=gpu # Run on the GPU nodes queue
 #SBATCH -A pa210903 # Accounting project
 
@@ -24,4 +24,4 @@ export HF_DATASETS_OFFLINE="1"
 
 # Launch the executable
 cd $HOME/projects/Data2Text
-srun python3.8 entry_point.py --config_file configs/aris/pretrain/pretrain_t5_small_all_tasks.yaml --job_type pretrain
+srun python3.8 entry_point.py --config_file configs/aris/pretrain/pretrain_t5_base_all_tasks.yaml --job_type pretrain
