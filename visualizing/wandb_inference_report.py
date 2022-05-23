e0e5cd967bcf607b3cce9bede5fa30d6aa6958bb
MikeXydas <mikexydas@gmail.com>
2022-05-23 13:41:52 +0300

    Removing punctuation before PARENT calculation

--- visualizing/wandb_inference_report.py
+++ visualizing/wandb_inference_report.py
@@ -42,8 +42,11 @@ class InferenceEvaluation:
                                                       references=[self.targets], lang="en")['f1'][0]
 
         try:
-            _, _, self.parent, _ = parent_calc(predictions=[self.predicted.lower().split()],
-                                               references=[target.lower().split() for target in self.targets],
+            clean_predicted = self.predicted.lower().translate(str.maketrans('', '', string.punctuation)).split()
+            clean_references = [target.lower().translate(str.maketrans('', '', string.punctuation)).split()
+                                for target in self.targets]
+            _, _, self.parent, _ = parent_calc(predictions=clean_predicted,
+                                               references=clean_references,
                                                tables=tables_to_parent_format([self.source]))
         except ValueError:
             self.parent = -1
