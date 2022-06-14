6f2b3bdac7fdb17685b7e6a0dc4864be66344e4a
MikeXydas <mikexydas@gmail.com>
2022-06-14 10:44:21 +0300

    Created verbalisation placeholder

--- app/backend/routers/queries.py
+++ app/backend/routers/queries.py
@@ -17,3 +17,8 @@ class Query(BaseModel):
 @router.post("/qr2t_back/explain_query")
 async def explain_query(query: Query, query_explainer=Depends(get_query_explainer)):
     return {"explanation": await query_explainer.explain_query_results(query.conn_url, query.query, query.nl_query)}
+
+
+@router.post("/qr2t_back/explain_query_placeholder")
+async def explain_query_placeholder(query: Query, query_explainer=Depends(get_query_explainer)):
+    return {"explanation": "This is a placeholder verbalisation of the query"}
