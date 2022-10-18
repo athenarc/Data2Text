b02b2e9cdd542d955438a1c2ee4c7c1b655325ae
MikeXydas <mikexydas@gmail.com>
2022-10-18 15:46:03 +0300

    Using simplified format on main app

--- app/backend/processing/process_query_results.py
+++ app/backend/processing/process_query_results.py
@@ -1,18 +1,47 @@
 from typing import Dict
 
+from dateutil.parser import parse
+
 from app.backend.processing.process_query.difficulty_check import \
     DifficultyNotImplemented
 
 
 def create_metadata(table_name, nl_query="") -> str:
-    page_title = f"<page_title> {table_name} </page_title>"
-    section_title = f"<section_title> {nl_query if nl_query != '' else table_name} </section_title>"
-
-    return page_title + " " + section_title
+    return f"<query> {nl_query} <table> {table_name} "
 
 
-def create_cell(col_name: str, col_value: str) -> str:
-    return f"<cell> {col_value} <col_header> {col_name} </col_header> </cell>"
+def create_cell(col_name: str, col_value: str, ind: int) -> str:
+    def is_numeric(cell: str) -> bool:
+        try:
+            _ = float(cell.replace(',', ''))
+            return True
+        except ValueError:
+            pass
+
+        try:
+            _ = float(cell.replace(' ', '').replace('-', ''))
+            return True
+        except ValueError:
+            pass
+
+        return False
+
+    def is_date(cell: str) -> bool:
+        try:
+            _ = parse(cell, fuzzy=False)
+            return True
+        except (ValueError, OverflowError):
+            return False
+
+    def extract_type(cell_value):
+        if is_numeric(cell_value):
+            return 'NUMERIC'
+        elif is_date(cell_value):
+            return 'DATE'
+        else:
+            return 'STRING'
+
+    return f"<col{ind}> {col_name} | {extract_type(str(col_value))} | {col_value} "
 
 
 def query_results_to_totto(query_results: Dict[str, str], nl_query):
@@ -22,8 +51,8 @@ def query_results_to_totto(query_results: Dict[str, str], nl_query):
     if len(query_results['rows']) > 1:
         raise DifficultyNotImplemented("Query results with multiple rows cannot be explained yet.")
 
-    for row in query_results['rows']:
+    for ind, row in enumerate(query_results['rows']):
         for header, val in zip(query_results['col_names'], row):
-            table_cells_str += create_cell(header, val) + " "
+            table_cells_str += create_cell(header, val, ind)
 
-    return f"{metadata} <table> {table_cells_str}</table>"
+    return metadata + table_cells_str
--- app/backend/routers/queries.py
+++ app/backend/routers/queries.py
@@ -19,8 +19,3 @@ class Query(BaseModel):
 async def explain_query(query: Query, query_explainer=Depends(get_query_explainer)):
     pick_database_interface(query.conn_url, query_explainer)
     return {"explanation": await query_explainer.explain_query_results(query.conn_url, query.query, query.nl_query)}
-
-
-@router.post("/qr2t_back/explain_query_placeholder")
-async def explain_query_placeholder(query: Query, query_explainer=Depends(get_query_explainer)):
-    return {"explanation": "This is a placeholder verbalisation of the query"}
