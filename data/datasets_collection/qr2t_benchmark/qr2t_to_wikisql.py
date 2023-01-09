7d3f9f8b8b80c275b8f9407b981baceada3952da
MikeXydas <mikexydas@gmail.com>
2023-01-09 12:27:22 +0200

    Training on Cordis annotations

--- data/datasets_collection/qr2t_benchmark/qr2t_to_wikisql.py
+++ data/datasets_collection/qr2t_benchmark/qr2t_to_wikisql.py
@@ -46,12 +46,20 @@ def read_all_annotations(annotations_dir):
 
 
 def create_splits(annotations, train_percentage=0.8):
-    # Order the annotations by number of annotations for each datapoint
-    # This way we can make sure that our evaluation dataset will have multiple annotations per datapoint
-    ordered_annotations = sorted(annotations, key=lambda annot: len(annot['annotations']))
+    # # Order the annotations by number of annotations for each datapoint
+    # # This way we can make sure that our evaluation dataset will have multiple annotations per datapoint
+    # ordered_annotations = sorted(annotations, key=lambda annot: len(annot['annotations']))
+    train_split = []
+    eval_split = []
 
-    train_split = ordered_annotations[:int(len(ordered_annotations) * train_percentage)]
-    eval_split = ordered_annotations[int(len(ordered_annotations) * train_percentage):]
+    for annot in annotations:
+        if len(annot['annotations']) > 1:
+            eval_split.append(annot)
+        else:
+            train_split.append(annot)
+
+    # train_split = ordered_annotations[:int(len(ordered_annotations) * train_percentage)]
+    # eval_split = ordered_annotations[int(len(ordered_annotations) * train_percentage):]
 
     for train_point in train_split:
         train_point['annotations'] = [train_point['annotations'][-1]]
@@ -77,17 +85,16 @@ def get_table_names(original_query):
     return ', '.join(find_from_tables(query['from']))
 
 
-def create_wiki_sql_datapoint(qr2t_datapoint):
-    table_names = get_table_names(qr2t_datapoint['data']['original_query'])
+def create_wiki_sql_datapoint(qr2t_datapoint, nl_queries_inv_index):
+    table_names = get_table_names(nl_queries_inv_index[qr2t_datapoint['data']['nl_query']])
     # print(qr2t_datapoint['annotations'])
     return {
         "table_id": table_names,
-        "query": qr2t_datapoint['data']['transformed_query'],
+        "query": nl_queries_inv_index[qr2t_datapoint['data']['nl_query']],
         "table_name": table_names,
         "query_description": qr2t_datapoint['data']['nl_query'],
         "results_description": [annot['result'][-1]['value']['text'][-1] for annot in qr2t_datapoint['annotations']],
-        "result": query_result_to_str(qr2t_datapoint['data']['res']),
-        "difficulty": qr2t_datapoint['data']['category']
+        "result": query_result_to_str(qr2t_datapoint['data']['inference'])
     }
 
 
@@ -97,7 +104,17 @@ def flatten_target_text(datapoints):
 
 
 def qr2t_to_wikisql(datapoints, store_path, flatten_target=False):
-    final_datapoints = [create_wiki_sql_datapoint(datapoint) for datapoint in datapoints]
+    all_original_datapoints = []
+
+    original_dataset_dir = 'storage/datasets/cordis_inode/original/json/'
+    for file in glob.glob(original_dataset_dir + '*'):
+        with open(file, 'r') as json_file:
+            all_original_datapoints += json.load(json_file)
+
+    nl_queries_inv_index = {original_datapoint['question']: original_datapoint['sql_query']
+                            for original_datapoint in all_original_datapoints}
+
+    final_datapoints = [create_wiki_sql_datapoint(datapoint, nl_queries_inv_index) for datapoint in datapoints]
 
     if flatten_target:
         flatten_target_text(final_datapoints)
@@ -107,12 +124,12 @@ def qr2t_to_wikisql(datapoints, store_path, flatten_target=False):
 
 
 if __name__ == '__main__':
-    ANNOTATIONS_DIR = 'storage/datasets/spider/annotations/label_studio/exports/04_18_2022__15_07_53/'
-    TARGET_DIR = 'storage/datasets/qr2t_benchmark/'
+    ANNOTATIONS_DIR = 'storage/datasets/cordis_inode/original/label_studio/labelled/12_28_2022__13_10_52/'
+    TARGET_DIR = 'storage/datasets/cordis_inode/to_wiki_sql/'
 
     annots = read_all_annotations(ANNOTATIONS_DIR)
 
-    train_split, eval_split = create_splits(annots)
+    train_split, eval_split = create_splits(annots, train_percentage=0.6)
 
     qr2t_to_wikisql(train_split, TARGET_DIR + 'train.json', flatten_target=True)
     qr2t_to_wikisql(eval_split, TARGET_DIR + 'dev.json', flatten_target=True)
